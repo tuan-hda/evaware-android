6124e5f7ed5cddb5be4925c3087cd711be5a8ed5
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-04-10 15:16:20 +0700

    Add bottom navigation view

--- app/src/main/java/com/example/evaware/presentation/base/MainActivity.java
+++ app/src/main/java/com/example/evaware/presentation/base/MainActivity.java
@@ -3,23 +3,36 @@ package com.example.evaware.presentation.base;
 import android.os.Bundle;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.core.splashscreen.SplashScreen;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.evaware.R;
+import com.example.evaware.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private com.example.evaware.databinding.ActivityMainBinding binding;
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = "MainActivity";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
 
-        binding = com.example.evaware.databinding.ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
     }
 }
