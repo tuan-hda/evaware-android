b3198386debc2f308d2559abc340d22c10486e3e
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-05-08 11:57:53 +0700

    Change to account

--- app/src/main/java/com/example/evaware/presentation/user/Account.java
+++ app/src/main/java/com/example/evaware/presentation/user/Account.java
@@ -1,7 +1,11 @@
 package com.example.evaware.presentation.user;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +13,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.evaware.R;
+import com.example.evaware.databinding.FragmentAccountBinding;
+import com.example.evaware.databinding.FragmentUserBinding;
+import com.example.evaware.presentation.auth.TestLoginActivity;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +28,8 @@ import com.example.evaware.R;
  * create an instance of this fragment.
  */
 public class Account extends Fragment {
+    FragmentAccountBinding binding;
+    FirebaseAuth auth;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +75,30 @@ public class Account extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        auth = FirebaseAuth.getInstance();
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUpButtons();
+    }
+
+    private void setUpButtons() {
+        binding.accountLlSignOut.setOnClickListener(view -> {
+            auth.signOut();
+            LoginManager.getInstance().logOut();
+            GoogleSignInOptions gso =
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+            GoogleSignInClient gsc = GoogleSignIn.getClient((Activity)getContext(), gso);
+            gsc.signOut();
+
+            Intent intent = new Intent(getActivity(), TestLoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            ((Activity)getContext()).startActivity(intent);
+            ((Activity)getContext()).finish();
+        });
     }
 }
