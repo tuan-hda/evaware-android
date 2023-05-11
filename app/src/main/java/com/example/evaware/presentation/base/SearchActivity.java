3be0318e44730fcb6ec9020615b0ed1124739a31
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-05-11 14:21:54 +0700

    Remove onClick attribute on search_layout

--- app/src/main/java/com/example/evaware/presentation/base/SearchActivity.java
+++ app/src/main/java/com/example/evaware/presentation/base/SearchActivity.java
@@ -1,19 +1,28 @@
 package com.example.evaware.presentation.base;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.evaware.R;
+import com.example.evaware.databinding.SearchBinding;
 
 public class SearchActivity extends AppCompatActivity {
-
-
+    private SearchBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
+        binding = SearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SearchActivity.this, "Hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
