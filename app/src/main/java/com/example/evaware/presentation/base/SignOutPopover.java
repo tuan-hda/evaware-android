c21a805f6c2a3bc93f6d9dd8c299aa7dab4b4350
UIT-20521857 <20521857@gm.uit.edu.vn>
2023-05-17 21:21:48 +0700

    Sign out popover

---
+++ app/src/main/java/com/example/evaware/presentation/base/SignOutPopover.java
@@ -0,0 +1,31 @@
+package com.example.evaware.presentation.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+
+import com.example.evaware.R;
+import com.example.evaware.databinding.ActivityLoginBinding;
+import com.example.evaware.databinding.ActivityMainBinding;
+
+
+public class SignOutPopover extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_out_popover);
+
+    }
+}
