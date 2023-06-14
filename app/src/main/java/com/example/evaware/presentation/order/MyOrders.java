d7dce5de5e9eae0e0d5204d17d3cd02a0503e090
ThanhTu-Phan <pttu2902@gmail.com>
2023-06-14 22:11:27 +0700

    commit to pull

--- app/src/main/java/com/example/evaware/presentation/order/MyOrders.java
+++ app/src/main/java/com/example/evaware/presentation/order/MyOrders.java
@@ -2,15 +2,23 @@ package com.example.evaware.presentation.order;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.evaware.R;
+import com.example.evaware.databinding.FragmentMyOrdersBinding;
+import com.example.evaware.databinding.FragmentSavedItemBinding;
 
 public class MyOrders extends Fragment {
+    FragmentMyOrdersBinding binding;
+    FragmentActivity activity;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +28,25 @@ public class MyOrders extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_orders, container, false);
+        activity = requireActivity();
+        binding = FragmentMyOrdersBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUpButtons();
+        setUpData();
+    }
+
+    private void setUpData() {
+    }
+
+    private void setUpButtons() {
+        binding.myOrdersIbReturn.setOnClickListener(view -> {
+            getActivity().getSupportFragmentManager().popBackStack();
+        });
     }
 }
